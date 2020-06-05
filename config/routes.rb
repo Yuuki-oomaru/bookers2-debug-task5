58440cd1903c8fce0fb7d0e8848a19df747e53fb
大丸　裕喜 <marumaru@oomaruhirokinoMacBook-Pro.local>
2020-06-05 16:52:11 +0900

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
     resource :favorites, only:[:create, :destroy]
     resources :book_comments, only:[:create, :destroy]
   end
-  resources :users
+  resources :users do
+    member do
+    get :following, :followers
+   end
+ end
+  resources :relationships, only: [:create, :destroy]
 
 end
